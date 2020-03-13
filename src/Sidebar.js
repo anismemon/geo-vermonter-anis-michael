6b03857ff87ee74f1cc240e6d022b5597814e4ec
foxflyer40 <60403841+foxflyer40@users.noreply.github.com>
2020-03-13 16:01:38 -0400

    sidebar layed out - needs styling

--- src/Sidebar.js
+++ src/Sidebar.js
@@ -15,16 +15,22 @@ class Sidebar extends React.Component {
                 <button className='compassButton' id='south' >S</button>
                 <button className='compassButton' id='west' >W</button>
 
-                <input type='text' readOnly="readonly" className='displayLocationData' id='latitude' placeholder='?' />Latitude
-                <input type='text' readOnly="readonly" className='displayLocationData' id='longitude' placeholder='?'/>Longitude
-                <input type='text' readOnly="readonly" className='displayLocationData' id='county' placeholder='?'/>County
-                <input type='text' readOnly="readonly" className='displayLocationData' id='town' placeholder='?'/>Town
-                <input type='text' readOnly="readonly" className='displayLocationData' id='score' placeholder={this.props.points}/>Score
+                <button className='button' id='returnButton' >Return</button>
 
+                <input type='text' readOnly="readonly" className='displayLocationData' id='score' placeholder={this.props.points}/>
+
+                <input type='text' readOnly="readonly" className='displayLocationData' id='latitude' placeholder='Latitude' />
+                <input type='text' readOnly="readonly" className='displayLocationData' id='longitude' placeholder='Longitude'/>
+                <input type='text' readOnly="readonly" className='displayLocationData' id='county' placeholder='County'/>
+                <input type='text' readOnly="readonly" className='displayLocationData' id='town' placeholder='Town'/>
+                
+                <input type='text' readOnly='readonly' className='displayLocationData' id='winCondition' placeholder='Correct // Wrong -10pts'/>
                 
                 <img id='vtCountyMap' src='https://geology.com/county-map/vermont-county-map.gif' alt='County Map' />
 
-                <button className='button' id='returnButton' >Return</button>
+                
+
+
 
 
             </div>
